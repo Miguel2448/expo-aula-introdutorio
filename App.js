0e305137f4e7e5d0d5f7a9f3966c74c363fc1b93
Miguel2448 <116905491+Miguel2448@users.noreply.github.com>
2023-08-04 11:14:09 +0000

    yes

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+  });
+
+  if (!fontsLoaded) {
+    return <View><Text>Não carregou a fonte</Text></View>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Miguel da Silva Vagula</Text>
+      <Text style={styles.title}>Miguel da Silva Vagula</Text>
       <StatusBar style="auto" />
     </View>
   );
