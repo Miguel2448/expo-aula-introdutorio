3b5ee45ad36945fcd258ed6b2123c461e4efd690
Miguel2448 <116905491+Miguel2448@users.noreply.github.com>
2023-08-18 11:32:55 +0000

    moedas

--- App.js
+++ App.js
@@ -1,19 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useFonts } from 'expo-font';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { useState } from 'react';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    'Inter-Black': require('./assets/fonts/Inter-Black.ttf'),
-  });
+  const [moedaOrigem, setMoedaOrigem] = useState('BRL')
+  const [moedaDestino, setMoedaDestino] = useState('USD')
+  const [valorEntrada, setVAlorEntrada] = useState('33.33')
+  const [resultado, setResultado] = useState('')
 
-  if (!fontsLoaded) {
-    return <View><Text>Não carregou a fonte</Text></View>;
+  const handleConverter = async () => {
+  let URL = `https://economia.awesomeapi.com.br/last/${moedaOrigem}-${moedaDestino}`;
+  try {
+    let page = await fetch(URL);
+    let json = await page.json();
+    //console.log(json);
+    let indice = parseFloat(json[`${moedaOrigem}${moedaDestino}`].high)
+    //setResultado(indice)
+    setResultado((indice*valor).toFixed(2))
+  } catch (error) {
+    setResultado(`Erro: ${error.message}`)
+  }
+  }
+
+  const handleLimpar = () => {
+    setResultado('');
+    setVAlorEntrada('33.33333');
+    setMoedaOrigem('BRL');
+    setMoedaDestino('USD');
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Miguel da Silva Vagula</Text>
+      <Text style={styles.title}>Conversor de Moedas</Text>
+      <View>
+        <Text style={styles.tbMoeda}>Moeda 1</Text>
+        <Picker
+        style={styles.picker}
+          selectedValue={moedaOrigem}
+          onValueChange={(itemValue, itemIndex) =>
+            setMoedaOrigem(itemValue)
+          }>
+          <Picker.Item label="Real Brasileiro" value="BRL" />
+          <Picker.Item label="Dólar Americano" value="USD" />
+          <Picker.Item label="Bitcoin" value="BTC" />
+          <Picker.Item label="Dogecoin" value="DOGE" />
+          <Picker.Item label="Ethereum" value="ETH" />
+          <Picker.Item label="Iene Japonês" value="JPY" />
+        </Picker>
+      </View>
+      <View>
+        <Text style={styles.tbMoeda}>Moeda 2</Text>
+        <Picker
+        style={styles.picker}
+          selectedValue={moedaDestino}
+          onValueChange={(itemValue, itemIndex) =>
+            setMoedaDestino(itemValue)
+          }>
+          <Picker.Item label="Real Brasileiro" value="BRL" />
+          <Picker.Item label="Dólar Americano" value="USD" />
+          <Picker.Item label="Bitcoin" value="BTC" />
+          <Picker.Item label="Dogecoin" value="DOGE" />
+          <Picker.Item label="Ethereum" value="ETH" />
+          <Picker.Item label="Iene Japonês" value="JPY" />
+        </Picker>
+      </View>
+      <View>
+        <Text style={styles.tbMoeda}>Valor para conversão</Text>
+        <TextInput
+        style={styles.input}
+        value={valorEntrada}
+        onChangeText={setVAlorEntrada}
+        keyboardType='numeric'>
+        </TextInput>
+      </View>
+      <Pressable style={styles.button}>
+        <Text style={styles.title}>Converter</Text>
+      </Pressable>
+      <View>
+        <Text>Resultado</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -28,7 +94,25 @@ const styles = StyleSheet.create({
   },
   title: {
     color: '#fff',
-    fontFamily: 'Inter-Black',
-    fontSize: 30,
   },
-});
+  picker: {
+    color: '#fff',
+    width: '200px',
+    height:'50px',
+    backgroundColor: '#000',
+  },
+  input: {
+    color: '#fff',
+    textAlign: 'right',
+    height: 40,
+    width: 200
+
+  },
+  tbMoeda: {
+    color: '#fff',
+  },
+  button: {
+    width: 200,
+    height: 40,
+  },
+})
